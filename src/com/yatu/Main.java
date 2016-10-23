6cae7de7d329ae442473c4a92e56eb3f6204e1f7
yatun <yatu@hudongpai.com>
2016-10-23 12:48:11 +0800

    init

--- src/com/yatu/Main.java
+++ src/com/yatu/Main.java
@@ -3,7 +3,7 @@ package com.yatu;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    System.out.print("nimei");
+        System.out.print(10&12);
+        
     }
 }
---
+++ src/com/yatu/Test.java
@@ -0,0 +1,7 @@
+package com.yatu;
+
+/**
+ * Created by Yatu on 2016/10/23.
+ */
+public class Test {
+}
